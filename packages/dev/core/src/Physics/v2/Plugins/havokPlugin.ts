a7fa44c60ac19e64b922d620f50e441f6e4dde30
Ara Adkins <me@ara.io>
2023-05-19 09:35:25 -0600

    Version update 6.4.1

--- packages/dev/core/src/Physics/v2/Plugins/havokPlugin.ts
+++ packages/dev/core/src/Physics/v2/Plugins/havokPlugin.ts
@@ -513,8 +513,23 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
                 const bodyTranslation = bodyTransform[0];
                 const bodyOrientation = bodyTransform[1];
                 const quat = TmpVectors.Quaternion[0];
-                transformNode.position.set(bodyTranslation[0], bodyTranslation[1], bodyTranslation[2]);
+
                 quat.set(bodyOrientation[0], bodyOrientation[1], bodyOrientation[2], bodyOrientation[3]);
+
+                const parent = transformNode.parent as TransformNode;
+                // transform position/orientation in parent space
+                if (parent && parent.absoluteRotationQuaternion) {
+                    TmpVectors.Vector3[2].set(bodyTranslation[0], bodyTranslation[1], bodyTranslation[2]);
+                    parent.absoluteRotationQuaternion.conjugateToRef(TmpVectors.Quaternion[1]);
+                    quat.multiplyInPlace(TmpVectors.Quaternion[1]);
+                    TmpVectors.Vector3[2].subtractToRef(parent.absolutePosition, TmpVectors.Vector3[0]);
+                    const localPosition = TmpVectors.Vector3[1];
+                    TmpVectors.Vector3[0].rotateByQuaternionToRef(TmpVectors.Quaternion[1], localPosition);
+                    transformNode.position.set(localPosition.x, localPosition.y, localPosition.z); // use set so position._isDirty is true
+                } else {
+                    transformNode.position.set(bodyTranslation[0], bodyTranslation[1], bodyTranslation[2]);
+                }
+
                 if (transformNode.rotationQuaternion) {
                     transformNode.rotationQuaternion.copyFrom(quat);
                 } else {
@@ -1149,6 +1164,11 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
      * It then creates an array containing the position and orientation of the node and returns it.
      */
     private _getTransformInfos(node: TransformNode): any[] {
+        if (node.parent) {
+            node.computeWorldMatrix();
+            return [this._bVecToV3(node.absolutePosition), this._bQuatToV4(node.absoluteRotationQuaternion)];
+        }
+
         let orientation = TmpVectors.Quaternion[0];
         if (node.rotationQuaternion) {
             orientation = node.rotationQuaternion;
@@ -1290,12 +1310,20 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
         const pivotA = options.pivotA ? this._bVecToV3(options.pivotA) : this._bVecToV3(Vector3.Zero());
         const axisA = options.axisA ?? new Vector3(1, 0, 0);
         const perpAxisA = this._tmpVec3[0];
-        axisA?.getNormalToRef(perpAxisA);
+        if (options.perpAxisA) {
+            perpAxisA.copyFrom(options.perpAxisA);
+        } else {
+            axisA.getNormalToRef(perpAxisA);
+        }
         this._hknp.HP_Constraint_SetAnchorInParent(jointId, pivotA, this._bVecToV3(axisA), this._bVecToV3(perpAxisA));
         const pivotB = options.pivotB ? this._bVecToV3(options.pivotB) : this._bVecToV3(Vector3.Zero());
         const axisB = options.axisB ?? new Vector3(1, 0, 0);
         const perpAxisB = this._tmpVec3[0];
-        axisB.getNormalToRef(perpAxisB);
+        if (options.perpAxisB) {
+            perpAxisB.copyFrom(options.perpAxisB);
+        } else {
+            axisB.getNormalToRef(perpAxisB);
+        }
         this._hknp.HP_Constraint_SetAnchorInChild(jointId, pivotB, this._bVecToV3(axisB), this._bVecToV3(perpAxisB));
 
         if (type == PhysicsConstraintType.LOCK) {
@@ -1435,7 +1463,7 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
      *
      */
     public getAxisFriction(constraint: PhysicsConstraint, axis: PhysicsConstraintAxis): number {
-        return this._hknp.HP_Constraint_GetAxisfriction(constraint._pluginData, this._constraintAxisToNative(axis))[1];
+        return this._hknp.HP_Constraint_GetAxisFriction(constraint._pluginData, this._constraintAxisToNative(axis))[1];
     }
 
     /**
@@ -1604,6 +1632,9 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
     public raycast(from: Vector3, to: Vector3, result: PhysicsRaycastResult): void {
         const queryMembership = ~0;
         const queryCollideWith = ~0;
+
+        result.reset(from, to);
+
         const query = [this._bVecToV3(from), this._bVecToV3(to), [queryMembership, queryCollideWith]];
         this._hknp.HP_World_CastRayWithCollector(this.world, this._queryCollector, query);
 
@@ -1617,8 +1648,6 @@ export class HavokPlugin implements IPhysicsEnginePluginV2 {
             const hitBody = this._bodies.get(hitData[1][0][0]);
             result.body = hitBody?.body;
             result.bodyIndex = hitBody?.index;
-        } else {
-            result.reset();
         }
     }
 
